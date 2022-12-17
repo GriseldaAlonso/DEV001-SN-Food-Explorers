cd98a4ecb458678625b9cd5e81a4bb65b15e2a93
GriseldaAlonso <gri_alonso@hotmail.com>
2022-12-16 19:29:56 -0500

    register con mensajes de error, conectado a firebase, funcionando

--- src/lib/JS/index.js
+++ src/lib/JS/index.js
@@ -13,13 +13,11 @@ export const myFunction = () => {
 
 export const createWithEmail = async (email, password) => {
   try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    // console.log(userCredentials);
+    const credentials = await createUserWithEmailAndPassword(auth, email, password);
+    return credentials;
   } catch (error) {
     const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
+    return errorCode;
   }
 };
 
@@ -28,12 +26,9 @@ export const createWithEmail = async (email, password) => {
 export const signInEmail = async (email, password) => {
   try {
     const credentials = await signInWithEmailAndPassword(auth, email, password);
-    console.log(credentials);
     return credentials;
-    // console.log(credentials);
   } catch (error) {
     const errorCode = error.code;
-    console.log(errorCode);
     return errorCode;
   }
 };
